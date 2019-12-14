dbe9ebac7d4fd8d239cb412266f9ef7421029484
Kenichi Saito <knchst0704@gmail.com>
2019-12-15 00:59:18 +0900

    Initial Commit

---
+++ ZoomableTransition/ZoomableTransition.h
@@ -0,0 +1,19 @@
+//
+//  ZoomableTransition.h
+//  ZoomableTransition
+//
+//  Created by Kenichi Saito on 12/15/19.
+//  Copyright © 2019 Kenichi Saito. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZoomableTransition.
+FOUNDATION_EXPORT double ZoomableTransitionVersionNumber;
+
+//! Project version string for ZoomableTransition.
+FOUNDATION_EXPORT const unsigned char ZoomableTransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZoomableTransition/PublicHeader.h>
+
+
